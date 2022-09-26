444fd63ad12ad66bc5d16a6cb725903180511db4
trmorningstar <trmorningstar@gmail.com>
2022-09-26 18:04:22 -0400

    add ticket schema

--- models/flight.js
+++ models/flight.js
@@ -3,9 +3,21 @@ import mongoose from 'mongoose'
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 	
+const ticketSchema = new Schema({
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number, min: 0
+  }, 
+})
+
+
 const flightSchema = new Schema({
   airline: {
     type: String, 
+
     enum: ['Delta', 'TWA', 'Spirit']
   },
   flightNo: {
@@ -17,6 +29,7 @@ const flightSchema = new Schema({
   departs: {
     type: Date, required: true
   },
+  tickets: [ticketSchema],
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
